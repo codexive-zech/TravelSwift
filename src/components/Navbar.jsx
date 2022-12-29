19100baf21e61fd4d51c19b0549ea8752cc4d005
Codexive_zech <zechariahhounwanou@gmail.com>
2022-12-30 00:38:18 +0100

    Implemented sticky navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { navLinks } from "../utils/data";
 import logo from "../assets/images/logo.png";
 import menu from "../assets/images/menu.svg";
@@ -7,15 +7,38 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   const [popUp, setPopUp] = useState(false);
+  const [scrollNav, setScrollNav] = useState(false);
+
+  const triggerNavScroll = () => {
+    if (window.scrollY > 80) {
+      setScrollNav(true);
+    } else {
+      setScrollNav(false);
+    }
+  };
 
   const changePopUp = () => {
     setPopUp(!popUp);
   };
+
+  useEffect(() => {
+    window.addEventListener("scroll", triggerNavScroll);
+    return () => {
+      window.removeEventListener("scroll", triggerNavScroll);
+    };
+  }, [scrollNav]);
+
   return (
     <>
-      <header className="absolute top-5 left-0 right-0">
+      <header
+        className={`${
+          scrollNav
+            ? " fixed h-[11vh] bg-white/80 opacity-100 z-30 top-0 right-0 left-0 backdrop-filter backdrop-blur-sm bg-opacity-20 bg-clip-padding shadow-sm shadow-slate-200 py-2 transition-all duration-200"
+            : "fixed top-5 left-0 right-0"
+        }`}
+      >
         <nav className="flex items-center justify-between travigo-container">
-          <div className="flex items-center">
+          <div className="flex items-center justify-center">
             <img src={logo} alt="logo" className=" w-20 h-14 object-fill" />
           </div>
           <ul className="flex items-center justify-between gap-7 lg:hidden">
